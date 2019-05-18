2cab258909cec9a91e570b2e93bd29c6a55dff6f
Anwesh43 <anweshthecool0@gmail.com>
2019-05-18 12:19:55 +0530

    adding State class

--- LoadingCircleStage.ts
+++ LoadingCircleStage.ts
@@ -101,3 +101,27 @@ class LoadingCircleStage {
         stage.handleTap()
     }
 }
+
+class State {
+
+    scale : number = 0
+    dir : number = 0
+    prevScale : number = 0
+
+    update(cb : Function) {
+        this.scale += ScaleUtil.updateValue(this.scale, this.dir, parts, parts)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
