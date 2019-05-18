5a0fe7a67cd2c792904d407fc5c5aa150f331c3c
Anwesh43 <anweshthecool0@gmail.com>
2019-05-18 12:33:24 +0530

    adding LoadingCircle class

--- LoadingCircleStage.ts
+++ LoadingCircleStage.ts
@@ -187,3 +187,27 @@ class LCNode {
         return this
     }
 }
+
+class LoadingCircle {
+
+    root : LCNode = new LCNode(0)
+    curr : LCNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
